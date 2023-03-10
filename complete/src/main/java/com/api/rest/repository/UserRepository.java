da79d06982af2233dd31ebd126875fff472994c8
Kamil <kamil.dul@gmail.com>
2023-03-10 21:00:40 +0100

    REST

---
+++ complete/src/main/java/com/api/rest/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.api.rest.repository;
+
+import com.api.rest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
